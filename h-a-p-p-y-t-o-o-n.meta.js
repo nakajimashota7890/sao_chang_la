8bf779be50e60729be0ca860b995dafd769248d8
nakajimashota7890 <nakajimashota7890@github.com>
2024-08-19 00:11:14 +0700

    init

---
+++ h-a-p-p-y-t-o-o-n.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         h-a-p-p-y-t-o-o-n
+// @namespace    https://github.com/nakajimashota7890/sao_chang_la/raw/main/h-a-p-p-y-t-o-o-n.user.js
+// @version      2024.08.19
+// @description  down h-a-p-p-y-t-o-o-n
+// @author       You
+// @match        https://happytoon01.com/webtoon/view/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=happytoon01.com
+// @require      https://cdn.jsdelivr.net/npm/@zip.js/zip.js@2.7.48/dist/zip.min.js
+// @downloadURL  https://github.com/nakajimashota7890/sao_chang_la/raw/main/h-a-p-p-y-t-o-o-n.user.js
+// @updateURL    https://github.com/nakajimashota7890/sao_chang_la/raw/main/h-a-p-p-y-t-o-o-n.meta.js
+// ==/UserScript==
